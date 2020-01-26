e7133fa2bf2b783e7f90cc405cff69435be53a52
eric <btstarinfo@gmail.com>
2020-01-26 19:51:24 +0800

    add functions

--- utils.go
+++ utils.go
@@ -7,16 +7,83 @@ import (
 	"time"
 )
 
-func Round8(x float64) float64 {
-	s := fmt.Sprintf("%.8f", x)
-	y, _ := strconv.ParseFloat(s, 64)
-	return y
+func ToFloat64(v interface{}) float64 {
+	if v == nil {
+		return 0.0
+	}
+
+	switch v.(type) {
+	case float64:
+		return v.(float64)
+	case string:
+		vStr := v.(string)
+		vF, _ := strconv.ParseFloat(vStr, 64)
+		return vF
+	default:
+		panic("to float64 error.")
+	}
 }
 
-func Round4(x float64) float64 {
-	s := fmt.Sprintf("%.4f", x)
-	y, _ := strconv.ParseFloat(s, 64)
-	return y
+func ToInt(v interface{}) int {
+	if v == nil {
+		return 0
+	}
+
+	switch v.(type) {
+	case string:
+		vStr := v.(string)
+		vInt, _ := strconv.Atoi(vStr)
+		return vInt
+	case int:
+		return v.(int)
+	case float64:
+		vF := v.(float64)
+		return int(vF)
+	default:
+		panic("to int error.")
+	}
+}
+
+func ToUint64(v interface{}) uint64 {
+	if v == nil {
+		return 0
+	}
+
+	switch v.(type) {
+	case int:
+		return uint64(v.(int))
+	case float64:
+		return uint64((v.(float64)))
+	case string:
+		uV, _ := strconv.ParseUint(v.(string), 10, 64)
+		return uV
+	default:
+		panic("to uint64 error.")
+	}
+}
+
+func ToInt64(v interface{}) int64 {
+	if v == nil {
+		return 0
+	}
+
+	switch v.(type) {
+	case float64:
+		return int64(v.(float64))
+	default:
+		vv := fmt.Sprint(v)
+
+		if vv == "" {
+			return 0
+		}
+
+		vvv, err := strconv.ParseInt(vv, 0, 64)
+		if err != nil {
+			return 0
+		}
+
+		return vvv
+	}
 }
 
 func Float64Round(x float64, prec ...int) float64 {
